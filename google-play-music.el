ec38245ea1ec32ba867cd66cf395b1dfcf64f5f2
A.I <merrick@luois.me>
2016-11-10 01:12:04 +0800

    implment control

--- google-play-music.el
+++ google-play-music.el
@@ -20,18 +20,17 @@
      (websocket-open
       "ws://localhost:5672"
       :on-message (lambda (_websocket frame)
-                    (dispatch-message (websocket-frame-text frame))
-                    )))
+                    (dispatch-message (websocket-frame-text frame)))))
 
 (defun dispatch-message(msg)
   (setq jmsg (let ((json-object-type 'plist))
                (json-read-from-string msg)))
   (let ((channel (plist-get jmsg :channel))
         (payload (plist-get jmsg :payload)))
-    (if (string= channel "connect")
-        (onConnect payload))
-    (if (string= channel "track")
-        (track-changed payload))))
+    (cond ((string= channel "time") nil)
+          ((string= channel "connect") (connection-change payload))
+          ((string= channel "track") (track-changed payload))
+          (t (message "%S: %S" channel payload)))))
 
 (defun track-changed (track)
   (setq google-play-music-current-track
@@ -40,16 +39,38 @@
          "-"
          (plist-get track :artist))))
 
-(defconst ehlo
-  '(:namespace "connect" :method: "connect" :arguments '(google-play-music.el)))
+(defun connect(&optional arg)
+  (message "%S" arg)
+  (let ((msg '(:namespace "connect" :method "connect")))
+    (let* ((param (if (numberp arg) (number-to-string arg) arg))
+          (parg (if param
+                    (plist-put msg :arguments `("google-play-music.el", param))
+                  (plist-put msg :arguments '("google-play-music.el")))))
+      (message "%S" parg)
+      (websocket-send-text gpmdp (json-encode parg)))))
 
-(json-encode ehlo)
-(defun connect()
-  (websocket-send-text gpmdp (json-encode ehlo))
-  )
-(defun onConnect(resp)
-  (message "connect: %s", resp))
+(defun connection-change (resp)
+  (interactive)
+  (if (string= resp "CODE_REQUIRED")
+      (let ((code (read-string "Enter Code: ")))
+        (connect code))
+    (connect resp)))
 
+(connect)
+
+(defun play-pause()
+  (let ((msg '(:namespace "playback" :method "playPause" :requestID 1)))
+    (websocket-send-text gpmdp (json-encode msg))))
+(defun play-next()
+  (let ((msg '(:namespace "playback" :method "forward" :requestID 1)))
+    (websocket-send-text gpmdp (json-encode msg))))
+(defun play-previous()
+  (let ((msg '(:namespace "playback" :method "rewind" :requestID 1)))
+    (websocket-send-text gpmdp (json-encode msg))))
+
+(play-pause)
+(play-next)
+(play-previous)
 
 (provide 'google-play-music)
 ;;; google-play-music.el ends here

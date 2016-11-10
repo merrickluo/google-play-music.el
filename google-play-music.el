1abae8725923a71db1f8c5450e06f58930476b0b
A.I <merrick@luois.me>
2016-11-10 20:45:34 +0800

    add  documentation

--- google-play-music.el
+++ google-play-music.el
@@ -29,24 +29,28 @@
     nil))
 
 (defun google-play-music--update-mode-line()
+  "update mode line by adding play state and currently playing track"
   (setq google-play-music-mode-line-text
         (concat google-play-music--current-state " " google-play-music--current-track)))
 
 (defun google-play-music--update-state(state)
+  "save currently playing state"
   (setq google-play-music--current-state
         (if (eq state :json-false)
             google-play-music--paused-symbol
           google-play-music--playing-symbol))
   (google-play-music--update-mode-line))
 
-(defun google-play-music--track-changed (track)
+(defun google-play-music--update-track(track)
+  "save currently playing track"
   (lexical-let* ((title (plist-get track :title))
                  (artist (plist-get track :artist)))
     (setq google-play-music--current-track
           (concat title "-" artist))
     (google-play-music--update-mode-line)))
 
-(defun google-play-music--connection-change (resp)
+(defun google-play-music--connection-change(resp)
+  "handle connection auth"
   (if (string= resp "CODE_REQUIRED")
       (let ((code (read-string "Enter Code: ")))
         (google-play-music--connect code))
@@ -54,6 +58,7 @@
 
 
 (defun google-play-music--dispatch-message(msg)
+  "dispatch messages from websocket server"
   (setq jmsg (let ((json-object-type 'plist))
                (json-read-from-string msg)))
   (let ((channel (plist-get jmsg :channel))
@@ -61,17 +66,19 @@
     (cond ((string= channel "time") nil)
           ((string= channel "playState") (google-play-music--update-state payload))
           ((string= channel "connect") (google-play-music--connection-change payload))
-          ((string= channel "track") (google-play-music--track-changed payload)))))
+          ((string= channel "track") (google-play-music--update-track payload)))))
 
 (defun google-play-music--connect(&optional arg)
-  (let ((msg '(:namespace "connect" :method "connect")))
-    (let* ((param (if (numberp arg) (number-to-string arg) arg))
-          (parg (if param
-                    (plist-put msg :arguments `("google-play-music.el", param))
-                  (plist-put msg :arguments '("google-play-music.el")))))
-      (websocket-send-text google-play-music--ws (json-encode parg)))))
+  "send connect request"
+    (let ((msg '(:namespace "connect" :method "connect")))
+      (let* ((param (if (numberp arg) (number-to-string arg) arg))
+             (parg (if param
+                       (plist-put msg :arguments `("google-play-music.el", param))
+                     (plist-put msg :arguments '("google-play-music.el")))))
+        (websocket-send-text google-play-music--ws (json-encode parg)))))
 
 (defun google-play-music-start()
+  "make connection to google play music desktop player"
   (interactive)
   (unless (connectp)
     (setq google-play-music--ws
@@ -81,16 +88,19 @@
                          (google-play-music--dispatch-message (websocket-frame-text frame)))))))
 
 (defun google-play-music-play-pause()
+  "Play Pause"
   (interactive)
   (let ((msg '(:namespace "playback" :method "playPause" :requestID 1)))
     (websocket-send-text google-play-music--ws (json-encode msg))))
 
 (defun google-play-music-forward()
+  "Play next song"
   (interactive)
   (let ((msg '(:namespace "playback" :method "forward" :requestID 1)))
     (websocket-send-text google-play-music--ws (json-encode msg))))
 
 (defun google-play-music-unwind()
+  "Rewind to start or previous song"
   (interactive)
   (let ((msg '(:namespace "playback" :method "rewind" :requestID 1)))
     (websocket-send-text google-play-music--ws (json-encode msg))))
